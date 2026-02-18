eeb405ebbdb94f01a6f1a56e1506253d5c6520dc
Yotsakon Mukda <note@192.168.1.94>
2026-02-18 16:35:34 +0700

    improve backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,13 +11,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL     
+];
 
 connectDB();
 
 app.use(express.json());
 app.use(cors({
-    origin: FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 }));
 
 app.use("/api/users", userRoutes);
